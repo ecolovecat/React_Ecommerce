b3feb9a16f63716ededf69ddf80a96ba90eda260
ecolovecat <tranphuongduy0403@gmail.com>
2023-02-25 11:43:18 +0700

    Visitor details

---
+++ project/app/Http/Controllers/VisitorController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Visitor;
+use Illuminate\Http\Request;
+
+class VisitorController extends Controller
+{
+    //
+    public function getVisitorDetails() {
+        $ip_address = $_SERVER['REMOTE_ADDR'];
+        date_default_timezone_set('Asia/Ho_Chi_Minh');
+        $visit_time = date("h:i:sa");
+        $visit_date = date("d-m-y");
+
+        $result = Visitor::insert([
+            'ip_address' => $ip_address,
+            'visit_time' => $visit_time,
+            'visit_date' => $visit_date
+        ]);
+        return $result;
+    }
+}
--- project/app/Models/Visitor.php
+++ project/app/Models/Visitor.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Visitor extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'ip_address',
+        'visit_time',
+        'visit_date',
+    ];
 }
