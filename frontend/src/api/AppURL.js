40adb22dc7f75b1a9a2c0409dc75b6bd67d5e915
ecolovecat <tranphuongduy0403@gmail.com>
2023-03-08 16:05:07 +0700

    render about page and purchase page

--- frontend/src/api/AppURL.js
+++ frontend/src/api/AppURL.js
@@ -2,7 +2,8 @@ const BaseURL = "http://127.0.0.1:8000/api"
 
 const AppURL = {
   VisitorDetails: BaseURL + "/getVisitor",
-  PostContact: BaseURL + "/postContact"
+  PostContact: BaseURL + "/postContact",
+  AllSiteInfo: BaseURL+"/allSiteInfo"
 };
 
 export default AppURL;
--- frontend/src/components/others/Purchase.jsx
+++ frontend/src/components/others/Purchase.jsx
@@ -1,23 +1,84 @@
-import React, { Component, Fragment } from 'react'
-import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import React, {Component, Fragment, useEffect, useState} from 'react'
+import {Container, Row, Col, Form, Button} from 'react-bootstrap'
+import AppURL from '../../api/AppURL';
+import axios from 'axios'
+import parse from "html-react-parser";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Purchase = (props) => {
-  return (
-    <Fragment>
-      <Container>
-        <Row className="p-2">
-          <Col className="shadow-sm bg-white mt-2" md={12} lg={12} sm={12} xs={12}>
-            <h4 className="section-title-login">Purchase Page </h4>
-            <p className="section-title-contact">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto iusto accusamus laudantium nulla necessitatibus quaerat, natus quibusdam officiis temporibus culpa consectetur magni tempore doloribus omnis hic expedita ab! Porro, quasi!
-            </p>
+    const [purchase, SetPurchase] = useState('')
+    const [loaderDiv, setLoaderDiv] = useState('');
+    const [mainDiv, setMainDiv] = useState('d-none');
+    useEffect(() => {
+        axios.get(AppURL.AllSiteInfo).then(response => {
+            let StatusCode = response.status;
+            if (StatusCode == 200) {
+                let JsonData = (response.data)[0]['purchase_guide'];
+                SetPurchase(JsonData);
+                setLoaderDiv("d-none")
+                setMainDiv("")
+            } else {
+              toast.error("Something went wrong.");
+            }
+
+        }).catch(error => {
+          console.log(error)
+        });
+    }, [])
+    return (
+        <Fragment>
+            <Container>
+                <Row className="p-2">
+                    <Col className="shadow-sm bg-white mt-2" md={12} lg={12} sm={12} xs={12}>
+                        <div className={loaderDiv}>
+
+                            <div className="ph-item">
+                                <div className="ph-col-12">
+                                    <div className="ph-row">
+                                        <div className="ph-col-4"></div>
+                                        <div className="ph-col-8 empty"></div>
+                                        <div className="ph-col-6"></div>
+                                        <div className="ph-col-6 empty"></div>
+                                        <div className="ph-col-12"></div>
+                                        <div className="ph-col-12"></div>
+                                        <div className="ph-col-12"></div>
+                                        <div className="ph-col-12"></div>
+                                    </div>
+                                </div>
+                            </div>
 
 
-          </Col>
-        </Row>
-      </Container>
-    </Fragment>
-  )
+                            <div className="ph-item">
+                                <div className="ph-col-12">
+                                    <div className="ph-row">
+                                        <div className="ph-col-4"></div>
+                                        <div className="ph-col-8 empty"></div>
+                                        <div className="ph-col-6"></div>
+                                        <div className="ph-col-6 empty"></div>
+                                        <div className="ph-col-12"></div>
+                                        <div className="ph-col-12"></div>
+                                        <div className="ph-col-12"></div>
+                                        <div className="ph-col-12"></div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+                        <div className={mainDiv}>
+                            <h4 className="section-title-login">Purchase Page </h4>
+                            <p className="section-title-contact">
+                                {parse(purchase)}
+                            </p>
+                        </div>
+
+
+                    </Col>
+                </Row>
+            </Container>
+        </Fragment>
+    )
 }
 
 export default Purchase
