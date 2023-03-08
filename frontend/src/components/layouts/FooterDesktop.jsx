a8f44dc61f74133584d6ad232262dbeac439f4bd
ecolovecat <tranphuongduy0403@gmail.com>
2023-03-08 17:33:38 +0700

    render dynamic infomation

--- frontend/src/components/layouts/FooterDesktop.jsx
+++ frontend/src/components/layouts/FooterDesktop.jsx
@@ -1,12 +1,43 @@
-import React, { Fragment, useEffect } from 'react'
+import React, {Fragment, useEffect, useState} from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import Apple from '../../assets/images/apple.png'
 import Google from '../../assets/images/google.png'
+import parse from "html-react-parser";
+import axios from 'axios'
+import AppURL from '../../api/AppURL';
 
 const FooterDesktop = (props) => {
+  const [address, setAddress] = useState('');
+  const [androidAppLink, setAndroidAppLink] = useState('');
+  const [iosAppLink, setIosAppLink] = useState('');
+  const [facebookLink, setFacebookLink] = useState('');
+  const [twitterLink, setTwitterLink] = useState('');
+  const [instagramLink, setInstagramLink] = useState('');
+  const [copyrightText, setCopyrightText] = useState('');
+  const [loaderDiv, setLoaderDiv] = useState('');
+  const [mainDiv, setMainDiv] = useState('d-none');
+
   useEffect(() => {
-    window.scroll(0, 0);
+    axios.get(AppURL.AllSiteInfo)
+        .then(response => {
+          let statusCode = response.status;
+          if (statusCode === 200) {
+            let jsonData = response.data[0];
+            setAddress(jsonData.address);
+            setAndroidAppLink(jsonData.android_app_link);
+            setIosAppLink(jsonData.ios_app_link);
+            setFacebookLink(jsonData.facbook_link);
+            setTwitterLink(jsonData.twitter_link);
+            setInstagramLink(jsonData.instagram_link);
+            setCopyrightText(jsonData.copyright_text);
+            setLoaderDiv('d-none');
+            setMainDiv('');
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        });
   }, []);
   return (
     <Fragment>
@@ -14,14 +45,37 @@ const FooterDesktop = (props) => {
         <Container>
           <Row className="px-0 my-5">
             <Col className="p-2" lg={3} md={3} sm={6} xs={12}>
-              <h5 className="footer-menu-title">OFFICE ADDRESS</h5>
-              <p>Hanoi, Cau Giay, Vietnam <br></br>
-                Email: ecolovecat@gmail.com
-              </p>
+              <div className={loaderDiv}>
+
+                <div className="ph-item">
+                  <div className="ph-col-12">
+                    <div className="ph-row">
+                      <div className="ph-col-4"></div>
+                      <div className="ph-col-8 empty"></div>
+                      <div className="ph-col-6"></div>
+                      <div className="ph-col-6 empty"></div>
+                      <div className="ph-col-12"></div>
+                      <div className="ph-col-12"></div>
+
+                    </div>
+                  </div>
+                </div>
+
+
+              </div>
+
+              <div className={mainDiv}>
+                <h5 className="footer-menu-title">OFFICE ADDRESS</h5>
+
+                {parse(address)}
+              </div>
+
               <h5 className="footer-menu-title">SOCIAL LINK</h5>
-              <a href=""><i className="fab m-1 h4 fa-facebook"></i></a>
-              <a href=""><i className="fab m-1 h4 fa-instagram"></i></a>
-              <a href=""><i className="fab m-1 h4 fa-twitter"></i></a>
+              <a href={facebookLink} target="_blank"><i className="fab m-1 h4 fa-facebook"></i></a>
+
+              <a href={instagramLink} target="_blank"><i className="fab m-1 h4 fa-instagram"></i></a>
+
+              <a href={twitterLink} target="_blank"><i className="fab m-1 h4 fa-twitter"></i></a>
             </Col>
 
             <Col className="p-2" lg={3} md={3} sm={6} xs={12}>
@@ -40,8 +94,9 @@ const FooterDesktop = (props) => {
 
             <Col className="p-2" lg={3} md={3} sm={6} xs={12}>
               <h5 className="footer-menu-title">DOWNLOAD APPS</h5>
-              <a><img src={Google} /></a><br></br>
-              <a><img className="mt-2" src={Apple} /></a><br></br>
+              <a href={androidAppLink} target="_blank"><img src={Google}  /></a><br></br>
+
+              <a href={iosAppLink} target="_blank"><img className="mt-2" src={Apple}  /></a><br></br>
               Change Your Language <br></br>
               <div id="google_translate_element">  </div>
             </Col>
@@ -52,7 +107,7 @@ const FooterDesktop = (props) => {
         <Container fluid={true} className="text-center m-0 pt-3 pb-1 bg-dark">
           <Container>
             <Row>
-              <h6 className="text-white">© Copyright 2021 by easy Shop, All Rights Reserved</h6>
+              <h6 className="text-white">{parse(copyrightText)}</h6>
             </Row>
           </Container>
         </Container>
