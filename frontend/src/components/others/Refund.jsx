a83f46ffff0ec94e3a77e8d2472b874b7cb53d53
ecolovecat <tranphuongduy0403@gmail.com>
2023-03-08 16:13:29 +0700

    render refund page

--- frontend/src/components/others/Refund.jsx
+++ frontend/src/components/others/Refund.jsx
@@ -1,16 +1,77 @@
-import React, { Component, Fragment } from 'react'
+import React, {Component, Fragment, useEffect, useState} from 'react'
 import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import axios from "axios";
+import AppURL from "../../api/AppURL";
+import {toast} from "react-toastify";
+import parse from "html-react-parser";
 
 const Refund = (props) => {
+  const [refund, setRefund] = useState('')
+  const [loaderDiv, setLoaderDiv] = useState('');
+  const [mainDiv, setMainDiv] = useState('d-none');
+
+  useEffect(() => {
+    axios.get(AppURL.AllSiteInfo).then(response =>{
+      let StatusCode = response.status;
+      if(StatusCode==200){
+        let JsonData = (response.data)[0]['refund'];
+        setRefund(JsonData);
+        setLoaderDiv("d-none")
+        setMainDiv("")
+      }
+
+    }).catch(error=>{
+      toast.error("Something went wrong.");
+    });
+  }, [])
+
+
   return (
     <Fragment>
       <Container>
         <Row className="p-2">
           <Col className="shadow-sm bg-white mt-2" md={12} lg={12} sm={12} xs={12}>
-            <h4 className="section-title-login">Refund Page </h4>
-            <p className="section-title-contact">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique voluptatem, tempore repellendus delectus fuga optio numquam, repudiandae aspernatur quia vel eveniet mollitia fugit voluptatibus id hic animi ratione expedita? Libero!
-            </p>
+            <div className={loaderDiv}>
+
+              <div className="ph-item">
+                <div className="ph-col-12">
+                  <div className="ph-row">
+                    <div className="ph-col-4"></div>
+                    <div className="ph-col-8 empty"></div>
+                    <div className="ph-col-6"></div>
+                    <div className="ph-col-6 empty"></div>
+                    <div className="ph-col-12"></div>
+                    <div className="ph-col-12"></div>
+                    <div className="ph-col-12"></div>
+                    <div className="ph-col-12"></div>
+                  </div>
+                </div>
+              </div>
+
+
+              <div className="ph-item">
+                <div className="ph-col-12">
+                  <div className="ph-row">
+                    <div className="ph-col-4"></div>
+                    <div className="ph-col-8 empty"></div>
+                    <div className="ph-col-6"></div>
+                    <div className="ph-col-6 empty"></div>
+                    <div className="ph-col-12"></div>
+                    <div className="ph-col-12"></div>
+                    <div className="ph-col-12"></div>
+                    <div className="ph-col-12"></div>
+                  </div>
+                </div>
+              </div>
+
+
+            </div>
+            <div className={mainDiv}>
+              <h4 className="section-title-login">Refund Page </h4>
+              <p className="section-title-contact">
+                {parse(refund)}
+              </p>
+            </div>
 
 
           </Col>
