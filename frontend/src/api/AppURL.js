6bec01b52a382cfd1e27448ce10bca533f055d35
ecolovecat <tranphuongduy0403@gmail.com>
2023-03-09 21:03:13 +0700

    render category

--- frontend/src/api/AppURL.js
+++ frontend/src/api/AppURL.js
@@ -3,7 +3,8 @@ const BaseURL = "http://127.0.0.1:8000/api"
 const AppURL = {
   VisitorDetails: BaseURL + "/getVisitor",
   PostContact: BaseURL + "/postContact",
-  AllSiteInfo: BaseURL+"/allSiteInfo"
+  AllSiteInfo: BaseURL + "/allSiteInfo",
+  AllCategories: BaseURL + "/allCategories",
 };
 
 export default AppURL;
--- frontend/src/components/home/Categories.jsx
+++ frontend/src/components/home/Categories.jsx
@@ -1,112 +1,45 @@
-import React, { Fragment } from 'react';
-import { Container, Row, Col, Card } from 'react-bootstrap'
-import "../../assets/css/home/featureProduct.scss"
-import "../../assets/css/home/card.scss"
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import AppURL from '../../api/AppURL';
+import axios from 'axios';
 
-function FeatureProduct() {
-  return (
-    <Fragment>
-      <div className="sb-feature-title">
-        <hr />
-        <h2>Categories</h2>
-        <hr />
-        <p className="sb-feature-p">
-          Paragraph goes here.
-        </p>
-      </div>
-      <Container>
-        <Row>
-          <Col key={1} xl={6} lg={6} md={2} sm={12} xs={12}>
-            <Row>
-              <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                <Card className="h-100 w-100 text-center sb-category-card">
-                  <Card.Body>
-                    <img className="center" src="https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100" />
-                    <h5 className="category-name">Historical</h5>
-                  </Card.Body>
-                </Card>
-              </Col>
+const Categories = () => {
+    const [menuData, setMenuData] = useState([]);
 
-              <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                <Card className="h-100 w-100 text-center">
-                  <Card.Body>
-                    <img className="center" src="https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100" />
-                    <h5 className="category-name">Bildungsroman</h5>
-                  </Card.Body>
-                </Card>
-              </Col>
+    useEffect(() => {
+        axios
+            .get(AppURL.AllCategories)
+            .then((response) => {
+                setMenuData(response.data);
+            })
+            .catch((error) => {
+                console.log(error)
+            });
+    }, []);
 
+    const myView = menuData.map((catList, i) => (
+        <Col key={i.toString()} className="p-0" xl={2} lg={2} md={2} sm={6} xs={6}>
+            <Card className="h-100 w-100 text-center">
+                <Card.Body>
+                    <img className="center" src={catList.category_image} />
+                    <h5 className="category-name">{catList.category_name}</h5>
+                </Card.Body>
+            </Card>
+        </Col>
+    ));
 
-              <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                <Card className="h-100 w-100 text-center">
-                  <Card.Body>
-                    <img className="center" src="https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100" />
-                    <h5 className="category-name">Adventure stories</h5>
-                  </Card.Body>
-                </Card>
-              </Col>
+    return (
+        <>
+            <Container className="text-center" fluid={true}>
+                <div className="section-title text-center mb-55">
+                    <h2> CATEGORIES</h2>
+                    <p>Some Of Our Exclusive Collection, You May Like</p>
+                </div>
 
+                <Row>{myView}</Row>
+            </Container>
+        </>
+    );
+};
 
-              <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                <Card className="h-100 w-100 text-center">
-                  <Card.Body>
-                    <img className="center" src="https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100" />
-                    <h5 className="category-name">Fantasy</h5>
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Row>
-
-          </Col>
-
-
-
-          <Col key={1} xl={6} lg={6} md={2} sm={12} xs={12}>
-            <Row>
-              <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                <Card className="h-100 w-100 text-center">
-                  <Card.Body>
-                    <img className="center" src="https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100" />
-                    <h5 className="category-name">Humour and satire</h5>
-                  </Card.Body>
-                </Card>
-              </Col>
-
-              <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                <Card className="h-100 w-100 text-center">
-                  <Card.Body>
-                    <img className="center" src="https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100" />
-                    <h5 className="category-name">Poetry</h5>
-                  </Card.Body>
-                </Card>
-              </Col>
-
-
-              <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                <Card className="h-100 w-100 text-center">
-                  <Card.Body>
-                    <img className="center" src="https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100" />
-                    <h5 className="category-name">Short stories</h5>
-                  </Card.Body>
-                </Card>
-              </Col>
-
-
-              <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                <Card className="h-100 w-100 text-center">
-                  <Card.Body>
-                    <img className="center" src="https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100" />
-                    <h5 className="category-name">Beauty</h5>
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Row>
-
-          </Col>
-        </Row>
-      </Container>
-    </Fragment>
-  );
-}
-
-export default FeatureProduct;
+export default Categories;
