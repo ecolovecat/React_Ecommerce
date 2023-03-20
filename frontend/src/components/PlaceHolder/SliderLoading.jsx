99b96e5064aed116c73277cf305af277a6b3cfb5
ecolovecat <tranphuongduy0403@gmail.com>
2023-03-20 16:49:30 +0700

    Loading placeholder frontend

---
+++ frontend/src/components/PlaceHolder/SliderLoading.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const SliderLoading = (props) => {
+        let isLoading = props.isLoading;
+
+        return (
+            <div className={isLoading}>
+                <div className="row">
+                    <div className="col-3">
+                        <div class="ph-row">
+                            <div class="ph-col-12"></div>
+                            <div class="ph-col-12"></div>
+                            <div class="ph-col-12"></div>
+                            <div class="ph-col-12"></div>
+                            <div class="ph-col-12"></div>
+                            <div class="ph-col-12"></div>
+                            <div class="ph-col-12"></div>
+                            <div class="ph-col-12"></div>
+                        </div>
+                    </div>
+                    <div className="col-9">
+                        <div class="ph-picture"></div>
+                    </div>
+                </div>
+            </div>
+        )
+}
+
+export default SliderLoading
